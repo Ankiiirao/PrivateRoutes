7ffea6cb67a11fccb839806329e997628c5025c9
Ankita <ankitarao01011998@gmail.com>
2021-02-05 22:55:52 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import {Router,Switch,Route} from 'react-router-dom'
+import {BrowserRouter as Router,Switch,Route, Redirect} from 'react-router-dom'
 import Home from './components/Home'
 import Profile from './components/profile'
 import Dashboard from './components/Dashboard'
+
+
 function App() {
   return (
     <Router>
       <Switch>
+        <div>
+          <h1>Hello</h1>
+          <div>
+            {window.location.pathname=='/'&& <Redirect to='/Dashboard'/>}
           <Route path="/Dashboard" component={Dashboard}/>
           <Route path="/Profile" component={Profile}/>
           <Route path="/Home" component={Home}/>
+          </div>
+          <h1>welcome</h1>
+        </div>
       </Switch>
     </Router>
   );
--- src/signin.js
+++ src/signin.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import { Redirect } from 'react-router-dom'
 import './style/signIn.css'
 export default function SignIn() {
+    const [place,setPlace]=useState(false)
     const setting = () =>{
         localStorage.setItem("token","{name:ankita}")
+        setPlace(true)
     }
+    useEffect(() => {
+        console.log("22222222222222222222")
+            place?<Redirect to='/Dashboard'/>:console.log("________________________________")
+    }, [place])
+  
     return (
         <div className="Sign-In">
             <input className="inpt" type="text" placeholder="Email"></input>
